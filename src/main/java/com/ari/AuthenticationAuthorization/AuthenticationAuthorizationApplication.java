7b31b1b384413d64151db478bd8a45574fcb5d6e
Arangajanan Sivasubramaniam <arangajanan.nar@gmail.com>
2023-10-08 22:22:07 +0530

    proj

---
+++ src/main/java/com/ari/AuthenticationAuthorization/AuthenticationAuthorizationApplication.java
@@ -0,0 +1,15 @@
+package com.ari.AuthenticationAuthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAuthorizationApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AuthenticationAuthorizationApplication.class, args);
+		System.out.println("AuthenticationAuthorizationApplication");
+	}
+
+}
